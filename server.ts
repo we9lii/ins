c35d99c167dbe983ed7fc4d254712cfb6161fe87
we9lii <wesli808@gmail.com>
2026-03-01 14:01:37 +0300

    fix express 5 routing issue

--- server.ts
+++ server.ts
@@ -325,7 +325,7 @@ async function startServer() {
     app.use(vite.middlewares);
   } else {
     app.use(express.static(path.join(__dirname, 'dist')));
-    app.get('*', (req, res) => {
+    app.use((req, res) => {
       res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     });
   }
